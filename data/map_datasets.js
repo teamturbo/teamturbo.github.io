e51dceecc53dcf7b5bc40fae0d234772a13a0b03
Werner Klaus <csam9224@student.uibk.ac.at>
2018-06-06 18:44:50 +0200

    dataset gegliedert

--- data/map_datasets.js
+++ data/map_datasets.js
@@ -14,7 +14,18 @@ Datenobjekt für Nightlife Innbruck JSON Beispiel
 
 */
 
-const nightlife_ibkDaten = [{
+const nightlife_bf_ibkDaten = [{
+        titel: "BreakfastClub",
+        info: "Maria-Theresien-Straße 49",
+        metadaten: "https://www.breakfast-club.at/",
+        attribute1: "+43 512 307959",
+        attribute2: "kontakt@breakfast-club.at",
+        lat: 47.2638846,
+        lng: 11.3941364,
+    },  
+];
+
+const nightlife_shops_ibkDaten = [{
         titel: "BreakfastClub",
         info: "Maria-Theresien-Straße 49",
         metadaten: "https://www.breakfast-club.at/",
@@ -23,5 +34,15 @@ const nightlife_ibkDaten = [{
         lat: 47.2638846,
         lng: 11.3941364,
     },
-    
+];
+
+const nightlife_bars_ibkDaten = [{
+        titel: "BreakfastClub",
+        info: "Maria-Theresien-Straße 49",
+        metadaten: "https://www.breakfast-club.at/",
+        attribute1: "+43 512 307959",
+        attribute2: "kontakt@breakfast-club.at",
+        lat: 47.2638846,
+        lng: 11.3941364,
+    },
 ];
